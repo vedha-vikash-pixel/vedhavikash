8e9828c2376542d24f2b8f9674b5d7cd5d7508b2
Vedha Vikash <Vedha.Vikash@in.ey.com>
2022-10-22 02:11:46 +0530

    routing modifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
-import { StackinfoComponentComponent } from './stackinfo-component/stackinfo-component.component';
 import { HiComponent } from './hi/hi.component';
 import { SkillsComponent } from './skills/skills.component';
-import { MenubarComponent } from './menubar/menubar.component';
 import { ProjectsComponent } from './projects/projects.component';
 import {MatButtonModule} from '@angular/material/button';
 import { ContactComponent } from './contact/contact.component';
@@ -23,11 +21,9 @@ import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    StackinfoComponentComponent,
+    AppComponent,    
     HiComponent,
-    SkillsComponent,
-    MenubarComponent,
+    SkillsComponent,    
     ProjectsComponent,
     ContactComponent,
     AboutmeComponent,
